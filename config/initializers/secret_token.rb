e0f5730f3a410ab4686955b0e8943e2570904e0a
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-04-01 00:06:00 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClickrecGen::Application.config.secret_token = 'a4c55787a94a00c3f94240943ad18eebcb19c3efc37375278b44662a76300961e809395bd93ce67e3d9ee4bdeed1ae95e7222f293881b890541183d25bb8106a'
